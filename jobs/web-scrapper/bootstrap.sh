01c13896e733e47ceac2cc2d7fc43a6c4d08ce93
Samriddha Chatterjee <Samriddha.Chatterjee@hcl.com>
2018-01-11 14:18:10 +0530

    web shot

--- jobs/web-scrapper/bootstrap.sh
+++ jobs/web-scrapper/bootstrap.sh
@@ -12,6 +12,7 @@ sudo apt-get install build-essential -y
 cd ../
 git clone https://samriddhac@github.com/samriddhac/blogsearch.git
 cd blogsearch/jobs/web-scrapper
+chmod 777 *.sh
 npm install
 mkdir -p /home/ubuntu/shared
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-fc378d55.efs.us-west-2.amazonaws.com:/ /home/ubuntu/shared
