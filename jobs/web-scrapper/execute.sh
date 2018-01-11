55ec01a691879ab0c7a6c65434f78d5342ab814e
Samriddha Chatterjee <Samriddha.Chatterjee@hcl.com>
2018-01-11 12:19:47 +0530

    web shot

--- jobs/web-scrapper/execute.sh
+++ jobs/web-scrapper/execute.sh
@@ -3,7 +3,6 @@ cd /home/ubuntu/blogsearch/jobs/web-scrapper
 git reset --hard
 git pull
 npm install
-chmod 777 execute.sh
 rm -rf blogs.csv
 wget https://s3-us-west-2.amazonaws.com/blogsearch-csv-bucket/blogs.csv
 nohup node index.js blogs.csv /home/ubuntu/shared/opt/jobs/scrape/output/ >> /home/ubuntu/shared/opt/jobs/scrape/logs/scrapelog-`date +%Y%m%d%H%M%S`.log &
